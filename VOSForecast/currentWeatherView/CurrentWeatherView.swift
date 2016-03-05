9db3a90e4bf6e5a1f2b91ef1e129984fbb9d9eca
vjosullivan <vjosullivan@gmail.com>
2016-03-05 16:36:43 +0000

    Containerised Current Weather added.

---
+++ VOSForecast/currentWeatherView/CurrentWeatherView.swift
@@ -0,0 +1,21 @@
+//
+//  CurrentWeatherView.swift
+//  VOSForecast
+//
+//  Created by Vincent O'Sullivan on 05/03/2016.
+//  Copyright © 2016 Vincent O'Sullivan. All rights reserved.
+//
+
+import UIKit
+
+class CurrentWeatherView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ VOSForecast/currentWeatherView/CurrentWeatherViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CurrentWeatherViewController.swift
+//  VOSForecast
+//
+//  Created by Vincent O'Sullivan on 05/03/2016.
+//  Copyright © 2016 Vincent O'Sullivan. All rights reserved.
+//
+
+import UIKit
+
+class CurrentWeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
