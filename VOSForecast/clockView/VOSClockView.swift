cdbffbc4a922131404717e79727468f320123db9
vjosullivan <vjosullivan@gmail.com>
2016-03-07 10:26:57 +0000

    Completion of clock refactoring.

--- VOSForecast/clockView/VOSClockView.swift
+++ VOSForecast/clockView/VOSClockView.swift
@@ -46,16 +46,10 @@ class VOSClockView: UIView {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        commonInit()
     }
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        commonInit()
-    }
-
-    private func commonInit() {
-        backgroundColor = UIColor.clearColor()
     }
 
     override func drawRect(rect: CGRect) {
