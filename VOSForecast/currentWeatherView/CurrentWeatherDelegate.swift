bd3f889a915291ce2fdfa05d92706a681239686a
vjosullivan <vjosullivan@gmail.com>
2016-03-07 10:08:56 +0000

    Refactoring.  Removed clock from main VC.

---
+++ VOSForecast/currentWeatherView/CurrentWeatherDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  CurrentWeatherDelegate.swift
+//  VOSForecast
+//
+//  Created by Vincent O'Sullivan on 07/03/2016.
+//  Copyright © 2016 Vincent O'Sullivan. All rights reserved.
+//
+
+import Foundation
